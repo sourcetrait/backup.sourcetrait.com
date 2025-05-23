e94ecb00d88c6d7b6e0084da984bb03e0975c17b
roylaurie <roy.laurie.pub@sourcetrait.com>
2025-05-22 20:43:57 -0700

    asset copying automated. product description copy automated

--- generate.bash
+++ generate.bash
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-cargo generate --init --overwrite --path ../templates www/site/product/info --name backup-sourcetrait-com --values-file ./cargo-generate.values.toml
+#cargo generate --init --overwrite --path ../templates www/site/product/info --name backup-sourcetrait-com --values-file ./cargo-generate.values.toml
+cargo generate --init --overwrite --allow-commands --path ../templates www/site/product/info --name backup-sourcetrait-com --values-file ./cargo-generate.values.toml
